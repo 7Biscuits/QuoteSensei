9d20dde7221dd46d63889497f0d2d9b6bad5a051
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-08-08 01:36:46 +0530

    updates

---
+++ README.md
@@ -0,0 +1,15 @@
+# QuoteSensei 🤖
+A quote generator bot made for National Design & Coding Championship 2023
+
+## To start the api/server
+### cd api
+### yarn start
+
+## To start the react application
+## cd client
+## yarn start
+
+## To start the discord bot
+### cd discord
+### yarn deploy
+### yarn start
--- client/src/components/QuoteCard.jsx
+++ client/src/components/QuoteCard.jsx
@@ -5,15 +5,8 @@ const QuoteCard = ({ quote, person, image }) => {
                 <img class="w-44 h-52 m-auto my-4" src={image} alt={person} />
             </a>
             <div class="p-5 text-center">
-
                 <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{quote}</h5>
                 <p class="mb-3 font-normal text-gray-100">~ {person}</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
             </div>
         </div>
 
